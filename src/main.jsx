c399208a0efaaca9af58ca847a31704ba839d018
reazzz122 <tebza0129@gmail.com>
2026-03-09 17:18:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SecurityServicesWebsite from "./security_services_websites.jsx";
+import SecurityServicesWebsite from "./security_services_websites";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
