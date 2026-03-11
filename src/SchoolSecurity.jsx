4ab117654f0035890131ca6b805abcf9adce71a2
reazzz122 <tebza0129@gmail.com>
2026-03-11 04:09:07 +0200

    Update SchoolSecurity.jsx

--- src/SchoolSecurity.jsx
+++ src/SchoolSecurity.jsx
@@ -1,1 +1,22 @@
+export default function SchoolSecurity() {
+  return (
+    <div className="page">
 
+      <h1>School Security Solutions</h1>
+
+      <p>
+        Schools require stronger surveillance and access control
+        to protect students, staff, and property.
+      </p>
+
+      <ul>
+        <li>School CCTV installation</li>
+        <li>Gate monitoring systems</li>
+        <li>Perimeter surveillance</li>
+        <li>Existing system upgrades</li>
+        <li>Security reviews</li>
+      </ul>
+
+    </div>
+  );
+}
