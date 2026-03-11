02e68a9426207b975fd5cf9d60b05e55e686a034
reazzz122 <tebza0129@gmail.com>
2026-03-11 05:01:04 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home.jsx";
 import Cybersecurity from "./Cybersecurity.jsx";
 import CCTV from "./CCTV.jsx";
 import SchoolSecurity from "./SchoolSecurity.jsx";
 import Contact from "./Contact.jsx";
-
 import "./styles.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cybersecurity" element={<Cybersecurity />} />
@@ -20,7 +19,7 @@ function App() {
         <Route path="/schools" element={<SchoolSecurity />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
