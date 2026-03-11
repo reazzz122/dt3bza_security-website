fa9327989705898e63fb71cc080b750ecefd8111
reazzz122 <tebza0129@gmail.com>
2026-03-11 04:01:55 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SecurityServicesWebsite from "./security_services_websites";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Cybersecurity from "./Cybersecurity";
+import CCTV from "./CCTV";
+import SchoolSecurity from "./SchoolSecurity";
+import Contact from "./Contact";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SecurityServicesWebsite />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cybersecurity" element={<Cybersecurity />} />
+        <Route path="/cctv" element={<CCTV />} />
+        <Route path="/schools" element={<SchoolSecurity />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
