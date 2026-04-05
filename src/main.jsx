0c9a3adfaf114d542b14ff6a27b29cf15420f0b1
reazzz122 <tebza0129@gmail.com>
2026-04-05 14:14:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./Home.jsx";
 import Cybersecurity from "./Cybersecurity.jsx";
 import CCTV from "./CCTV.jsx";
 import SchoolSecurity from "./SchoolSecurity.jsx";
 import Contact from "./Contact.jsx";
+import DT3App from "./DT3App.jsx";
 import "./styles.css";
 
 function App() {
@@ -18,13 +18,12 @@ function App() {
         <Route path="/cctv" element={<CCTV />} />
         <Route path="/schools" element={<SchoolSecurity />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/dt3app" element={<DT3App />} />
       </Routes>
     </HashRouter>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 );
